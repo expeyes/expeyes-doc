d8078c10ee86b21ccdd9989b7dfc3ac58b7ea7af
Georges Khaznadar <georgesk@debian.org>
2018-04-26 18:19:47 +0200

    put the identification in a comment element

--- svg2ts.py
+++ svg2ts.py
@@ -6,10 +6,33 @@ gettext.bindtextdomain('expeyes-doc', os.path.join(os.path.abspath(__file__),"la
 gettext.textdomain('expeyes-doc')
 _ = gettext.gettext
 
+TSminimal="""\
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>@default</name>
+</context>
+</TS>
+"""
+
 def error(msg, parser):
     print(msg)
     parser.print_help()
     sys.exit(1)
+
+def warn(msg):
+    print(msg, file=sys.stdout)
+    return
+
+def identsOf(doc):
+    """
+    make the set of id attributes found in a TS file
+    @param doc an xml.dom.minidom instance
+    """
+    loc=doc.getElementsByTagName("location")
+    return {l.getAttribute("id") for l in loc}
+
 
 def getTheText(t):
     """
@@ -34,9 +57,9 @@ def extractStrings(args, parser):
             outTs=args.outTs
         else:
             if re.match(r".*\.svg$", args.inSvg[0], flags=re.I):
-                outTs=re.sub("\.svg$", ".ts", args.inSvg[0], flags=re.I)
+                outTs=re.sub("\.svg$", "_en.ts", args.inSvg[0], flags=re.I)
             else:
-                outTs=args.inSvg[0]+".ts"
+                outTs=args.inSvg[0]+"_en.ts"
             print(_("extracting strings from {} to {}").format(args.inSvg[0], outTs))
             try:
                 doc=xml.dom.minidom.parse(args.inSvg[0])
@@ -47,7 +70,41 @@ def extractStrings(args, parser):
             translatable={t.getAttribute("id"): getTheText(t) for t in texts}
             # ignore the empty strings
             translatable={key: translatable[key] for key in translatable if translatable[key]}
-            print (translatable)
+            if not os.path.exists(outTs):
+                open(outTs,"w").write(TSminimal)
+            tDoc=xml.dom.minidom.parse(outTs)
+            idents=identsOf(tDoc)
+            context=tDoc.getElementsByTagName("context")[0]
+            for i in translatable:
+                if i in idents:
+                    warn(_("{ident} was already defined in {file}").format(ident=i, file=outTs))
+                    continue
+                message=tDoc.createElement("message")
+                location=tDoc.createElement("location")
+                filename=tDoc.createAttribute("filename")
+                filename.value=args.inSvg[0]
+                location.setAttributeNode(filename)
+                message.appendChild(location)
+                source=tDoc.createElement("source")
+                text=tDoc.createTextNode(translatable[i])
+                source.appendChild(text)
+                message.appendChild(source)
+                translation=tDoc.createElement("translation")
+                text=tDoc.createTextNode("")
+                translation.appendChild(text)
+                message.appendChild(translation)
+                comment=tDoc.createElement("comment")
+                text=tDoc.createTextNode(i)
+                comment.appendChild(text)
+                message.appendChild(comment)
+                context.appendChild(message)
+            pretty=tDoc.toprettyxml(indent="  ")
+            result=""
+            for line in pretty.split("\n"):
+                line=line.rstrip()
+                if line:
+                    result+=line+"\n"
+            open(outTs,"w").write(result)
     return
 
 if __name__=="__main__":
