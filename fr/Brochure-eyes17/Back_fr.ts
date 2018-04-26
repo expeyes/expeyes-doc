59cb3fcc42f861697a88da710407a7410b8527c5
Georges Khaznadar <georgesk@debian.org>
2018-04-26 18:42:18 +0200

    first translation to FR

--- fr/Brochure-eyes17/Back_fr.ts
+++ fr/Brochure-eyes17/Back_fr.ts
@@ -1,97 +1,97 @@
-<?xml version="1.0" ?>
+<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS language="fr_FR" version="2.1">
-  <context>
+<TS version="2.1" language="fr_FR">
+<context>
     <name>@default</name>
     <message>
-      <location filename="en/Brochure-eyes17/Back.svg"/>
-      <source>Transistor Output Characteristics</source>
-      <translation>Caractéristiques de sortie d&apos;un transistor</translation>
-      <comment>flowPara3372</comment>
+        <location filename="en/Brochure-eyes17/Back.svg"/>
+        <source>Transistor Output Characteristics</source>
+        <comment>flowPara3372</comment>
+        <translation>Caractéristiques de sortie d&apos;un transistor</translation>
     </message>
     <message>
-      <location filename="en/Brochure-eyes17/Back.svg"/>
-      <source>Output characteristics of an NPN transistor are being obtained in this experiment. The collector voltage is swept using the programmable voltage output, PV1, and a series 1K resistor is used to limit the current as well as to act as a shunt to measure the voltage drop across the transistor's C &amp; E. Base current is set via another source, PV2, and a 100k resistor. Collector and base voltages are monitored using analog inputs A1 &amp; A2. </source>
-      <translation>Des caractéristiques de sortie d&apos;un transistor NPN sont obtenues dans cette expérience. La tension du collecteur est balayée à l&apos;aide de la sortie de tension programmable, PV1, et une résistance en série de 1 kΩ  est utilisée pour limiter le courant ainsi que comme shunt pour mesurer le courant CE. Le courant de base est réglé à partir d&apos;une autre source, PV2, à l&apos;aide d&apos;une résistance de 100 kΩ . Les tensions de collecteur et de base sont enregistrées à l&apos;aide des entrées A1 et A2. </translation>
-      <comment>flowPara3388</comment>
+        <location filename="en/Brochure-eyes17/Back.svg"/>
+        <source>Output characteristics of an NPN transistor are being obtained in this experiment. The collector voltage is swept using the programmable voltage output, PV1, and a series 1K resistor is used to limit the current as well as to act as a shunt to measure the voltage drop across the transistor&apos;s C &amp; E. Base current is set via another source, PV2, and a 100k resistor. Collector and base voltages are monitored using analog inputs A1 &amp; A2. </source>
+        <comment>flowPara3388</comment>
+        <translation>Des caractéristiques de sortie d&apos;un transistor NPN sont obtenues dans cette expérience. La tension du collecteur est balayée à l&apos;aide de la sortie de tension programmable, PV1, et une résistance en série de 1 kΩ  est utilisée pour limiter le courant ainsi que comme shunt pour mesurer le courant CE. Le courant de base est réglé à partir d&apos;une autre source, PV2, à l&apos;aide d&apos;une résistance de 100 kΩ . Les tensions de collecteur et de base sont enregistrées à l&apos;aide des entrées A1 et A2. </translation>
     </message>
     <message>
-      <location filename="en/Brochure-eyes17/Back.svg"/>
-      <source>Transient  Response of LCR Circuits</source>
-      <translation></translation>
-      <comment>flowPara3402</comment>
+        <location filename="en/Brochure-eyes17/Back.svg"/>
+        <source>Transient  Response of LCR Circuits</source>
+        <comment>flowPara3402</comment>
+        <translation>Réponse transitoire de circuits RLC</translation>
     </message>
     <message>
-      <location filename="en/Brochure-eyes17/Back.svg"/>
-      <source>ExpEYES can generate/monitor voltage signals as a function of time. Other physical parameters like temperature,</source>
-      <translation></translation>
-      <comment>flowPara3410</comment>
+        <location filename="en/Brochure-eyes17/Back.svg"/>
+        <source>ExpEYES can generate/monitor voltage signals as a function of time. Other physical parameters like temperature,</source>
+        <comment>flowPara3410</comment>
+        <translation>ExpEYES peut générer/enregistrer des signaux de tension en fonction du temps. Et aussi d&apos;autres paramètres physiques tels que la température,</translation>
     </message>
     <message>
-      <location filename="en/Brochure-eyes17/Back.svg"/>
-      <source>velocity, frequency  etc. also can be handled using appropriate sensor elements. Experiments are essentially a</source>
-      <translation></translation>
-      <comment>flowPara3412</comment>
+        <location filename="en/Brochure-eyes17/Back.svg"/>
+        <source>velocity, frequency  etc. also can be handled using appropriate sensor elements. Experiments are essentially a</source>
+        <comment>flowPara3412</comment>
+        <translation>la vitesse, la fréquence, etc. on peut aussi gérer des capteurs appropriés. Les expériences sont essentiellement une</translation>
     </message>
     <message>
-      <location filename="en/Brochure-eyes17/Back.svg"/>
-      <source>sequence of these operations. Every hardware feature of ExpEYES is  accessed using a well-documented Python function call.</source>
-      <translation></translation>
-      <comment>flowPara3414</comment>
+        <location filename="en/Brochure-eyes17/Back.svg"/>
+        <source>sequence of these operations. Every hardware feature of ExpEYES is  accessed using a well-documented Python function call.</source>
+        <comment>flowPara3414</comment>
+        <translation>séquence de ces opérations. On accède à toutes les possibilités d&apos;ExpEYES à l&apos;aide de fonctions Python bien documentées.</translation>
     </message>
     <message>
-      <location filename="en/Brochure-eyes17/Back.svg"/>
-      <source>The examples shown above are mostly electrical and electronics experiments but  experiments on Mechanics, Sound, Heat etc. are also documented in the user manual. The programs are written in Python, due to its simple syntax and availability of modules for scientific computation and graphics. </source>
-      <translation></translation>
-      <comment>flowPara3420</comment>
+        <location filename="en/Brochure-eyes17/Back.svg"/>
+        <source>The examples shown above are mostly electrical and electronics experiments but  experiments on Mechanics, Sound, Heat etc. are also documented in the user manual. The programs are written in Python, due to its simple syntax and availability of modules for scientific computation and graphics. </source>
+        <comment>flowPara3420</comment>
+        <translation>Les exemples montrés ci-dessus sont en majorité des expériences d&apos;électricité et d&apos;électronique, mais le manuel utilisateur décrit aussi des expériences de mécanique, acoustique, thermophysique, etc. Les programmes sont écrits en Python à cause de sa syntaxe simple et de la disponibilité de modules pour le calcul scientifique et les graphiques. </translation>
     </message>
     <message>
-      <location filename="en/Brochure-eyes17/Back.svg"/>
-      <source>ExpEYES is supported by the PHOENIX project of Inter-University Accelerator Centre. IUAC conducts training programs for teachers, once in every six months. To be informed about the announcement of training programs and new developments, you may join the Phoenix mailing list from the website www.epeyes.in, or subscribe to the facebook page</source>
-      <translation></translation>
-      <comment>flowPara3430</comment>
+        <location filename="en/Brochure-eyes17/Back.svg"/>
+        <source>ExpEYES is supported by the PHOENIX project of Inter-University Accelerator Centre. IUAC conducts training programs for teachers, once in every six months. To be informed about the announcement of training programs and new developments, you may join the Phoenix mailing list from the website www.epeyes.in, or subscribe to the facebook page</source>
+        <comment>flowPara3430</comment>
+        <translation>ExpEYES est supporté par le projet PHOENIX, de l&apos;Inter-University Accelerator Centre. L&apos;IUAC organise des sessions de formation pour les enseignants tous les six mois. Pour être informé des sessions de formation, rejoignez la liste de diffusion Phoenix sur le site web www.epeyes.in, ou inscrivez-vous sur la page Facebook</translation>
     </message>
     <message>
-      <location filename="en/Brochure-eyes17/Back.svg"/>
-      <source> The website also  contains a lot documentation on ExpEYES including videos and a user manual. It  has some  Python resources to support teaching physics and mathematics. Information on MicroHOPE, a micro-controller development kit and KuttyPy, a simple device to do  control/monitoring using python, are also available.</source>
-      <translation></translation>
-      <comment>flowPara3440</comment>
+        <location filename="en/Brochure-eyes17/Back.svg"/>
+        <source> The website also  contains a lot documentation on ExpEYES including videos and a user manual. It  has some  Python resources to support teaching physics and mathematics. Information on MicroHOPE, a micro-controller development kit and KuttyPy, a simple device to do  control/monitoring using python, are also available.</source>
+        <comment>flowPara3440</comment>
+        <translation> Le site web contient aussi beaucoup de documentation pour ExpEYES, dont des vidéos et un manuel utlisateur. Il a quelques ressource en Python pour faciliter l&apos;enseignement des paths et de la physique. On y trouve aussi des informations sur MicroHOPE, un kit de développement de microcontrôleur et KuttyPy, un appareil simple pour du contrôle et de la mesure à l&apos;aide de Python.</translation>
     </message>
     <message>
-      <location filename="en/Brochure-eyes17/Back.svg"/>
-      <source>Communication to the ExpEYES hardware is done using a Python library, providing functions for operations like reading/setting voltages. The example shows the code to study an inverting amplifier. A 200 Hz sine wave is set on WG. The channels A1 and A2 are captured and plotted, using matplotlib.</source>
-      <translation></translation>
-      <comment>flowPara3526</comment>
+        <location filename="en/Brochure-eyes17/Back.svg"/>
+        <source>Communication to the ExpEYES hardware is done using a Python library, providing functions for operations like reading/setting voltages. The example shows the code to study an inverting amplifier. A 200 Hz sine wave is set on WG. The channels A1 and A2 are captured and plotted, using matplotlib.</source>
+        <comment>flowPara3526</comment>
+        <translation>La communication avec le matériel d&apos;ExpEYES se fait à l&apos;aide d&apos;une bibliothèque Python, qui fournit des fonctions telles que le réglage et la lecture de tensions,. L&apos;exemple montre le code pour étudier un amplificateur inverseur. Un signal sinusoïdal de 200 Hz est défini sur WD. Les canaux A1 et A2 sont enregistrés et mis en graphique à l&apos;aide de matplotlib.</translation>
     </message>
     <message>
-      <location filename="en/Brochure-eyes17/Back.svg"/>
-      <source>180 degree out of phase AC voltages required for the  full wave rectifier are provided by WG and WG outputs. </source>
-      <translation></translation>
-      <comment>flowPara3538</comment>
+        <location filename="en/Brochure-eyes17/Back.svg"/>
+        <source>180 degree out of phase AC voltages required for the  full wave rectifier are provided by WG and WG outputs. </source>
+        <comment>flowPara3538</comment>
+        <translation>Les tensions en opposition de phase nécessaire pour le redressement double-alternance sont fournies par les sorties WG et WG. </translation>
     </message>
     <message>
-      <location filename="en/Brochure-eyes17/Back.svg"/>
-      <source>The input waveforms are  monitored on A1 and A2 and the output on A3. A load resistor must be connected from A3 to ground to obtain proper output waveforms. A capacitor can be connected in parallel to the load</source>
-      <translation></translation>
-      <comment>flowPara3542</comment>
+        <location filename="en/Brochure-eyes17/Back.svg"/>
+        <source>The input waveforms are  monitored on A1 and A2 and the output on A3. A load resistor must be connected from A3 to ground to obtain proper output waveforms. A capacitor can be connected in parallel to the load</source>
+        <comment>flowPara3542</comment>
+        <translation>Les signaux d&apos;entrée sont enregistrés sur A1 et A1 et la sortie sur A3. Une résistance de charge doit être connectée entre A3 et la masse pour obtenir les signaux corrects. Un condensateur peut être connecté en parallèle à la résistance</translation>
     </message>
     <message>
-      <location filename="en/Brochure-eyes17/Back.svg"/>
-      <source>resistor to observe the effect of filtering. </source>
-      <translation></translation>
-      <comment>flowPara3544</comment>
+        <location filename="en/Brochure-eyes17/Back.svg"/>
+        <source>resistor to observe the effect of filtering. </source>
+        <comment>flowPara3544</comment>
+        <translation>de charge pour observer l&apos;effet de filtrage. </translation>
     </message>
     <message>
-      <location filename="en/Brochure-eyes17/Back.svg"/>
-      <source>The effect of LCR elements on the phase and amplitudes of an applied sine wave is studied using a series LCR circuit. The red trace is the total voltage across LC and it goes to zero at resonance (it is not exactly zero due to the resistance of the inductor coil). It can be seen that the voltages across L and C are equal and out of  phase at this point.</source>
-      <translation></translation>
-      <comment>flowPara3569</comment>
+        <location filename="en/Brochure-eyes17/Back.svg"/>
+        <source>The effect of LCR elements on the phase and amplitudes of an applied sine wave is studied using a series LCR circuit. The red trace is the total voltage across LC and it goes to zero at resonance (it is not exactly zero due to the resistance of the inductor coil). It can be seen that the voltages across L and C are equal and out of  phase at this point.</source>
+        <comment>flowPara3569</comment>
+        <translation>L&apos;efet des éléments RLC sur la phase et l&apos;amplitude d&apos;un signal sinusoïdal est étudié avec un circuit RLC série. La trace rouge est la tension totale aux bornes de LC et elle passe par zéro à la résonance (elle n&apos;est pas tout à fait nulle à cause de la résistance du bobinage). On peut voir que les tensions aux bornes de L et C sont déphasées à ce point.</translation>
     </message>
     <message>
-      <location filename="en/Brochure-eyes17/Back.svg"/>
-      <source>the capacitor is captured and plotted. For 120mH and 0.1uF, the circuit is under damped as shown. The captured waveform is analyzed to extract the resonant frequency and damping factor, with an accuracy better than one percent.</source>
-      <translation></translation>
-      <comment>flowPara4277</comment>
+        <location filename="en/Brochure-eyes17/Back.svg"/>
+        <source>the capacitor is captured and plotted. For 120mH and 0.1uF, the circuit is under damped as shown. The captured waveform is analyzed to extract the resonant frequency and damping factor, with an accuracy better than one percent.</source>
+        <comment>flowPara4277</comment>
+        <translation>le condensateur est enregistré et mis en graphique. Pour 120 mH et 0,1 µF, le circuit est sous-amorti comme on voit. Le signal enregistré est analysé pour extraire la fréquence de résonance et le facteur d&apos;amortissement, avec une précision meilleure que 1%.</translation>
     </message>
-  </context>
+</context>
 </TS>
