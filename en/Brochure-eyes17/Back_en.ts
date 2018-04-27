b45d24e5102731578cba1f21d3d904b04cc25fe6
Georges Khaznadar <georgesk@debian.org>
2018-04-27 11:53:15 +0200

    updated English TS files

--- en/Brochure-eyes17/Back_en.ts
+++ en/Brochure-eyes17/Back_en.ts
@@ -96,89 +96,269 @@
     <message>
       <location filename="en/Brochure-eyes17/Back.svg"/>
       <source>Transistor Output Characteristics</source>
-      <translation></translation>
+      <translation/>
       <comment>flowPara3372</comment>
     </message>
     <message>
       <location filename="en/Brochure-eyes17/Back.svg"/>
       <source>Output characteristics of an NPN transistor are being obtained in this experiment. The collector voltage is swept using the programmable voltage output, PV1, and a series 1K resistor is used to limit the current as well as to act as a shunt to measure the voltage drop across the transistor's C &amp; E. Base current is set via another source, PV2, and a 100k resistor. Collector and base voltages are monitored using analog inputs A1 &amp; A2. </source>
-      <translation></translation>
+      <translation/>
       <comment>flowPara3388</comment>
     </message>
     <message>
       <location filename="en/Brochure-eyes17/Back.svg"/>
       <source>Transient  Response of LCR Circuits</source>
-      <translation></translation>
+      <translation/>
       <comment>flowPara3402</comment>
     </message>
     <message>
       <location filename="en/Brochure-eyes17/Back.svg"/>
       <source>ExpEYES can generate/monitor voltage signals as a function of time. Other physical parameters like temperature,</source>
-      <translation></translation>
+      <translation/>
       <comment>flowPara3410</comment>
     </message>
     <message>
       <location filename="en/Brochure-eyes17/Back.svg"/>
       <source>velocity, frequency  etc. also can be handled using appropriate sensor elements. Experiments are essentially a</source>
-      <translation></translation>
+      <translation/>
       <comment>flowPara3412</comment>
     </message>
     <message>
       <location filename="en/Brochure-eyes17/Back.svg"/>
       <source>sequence of these operations. Every hardware feature of ExpEYES is  accessed using a well-documented Python function call.</source>
-      <translation></translation>
+      <translation/>
       <comment>flowPara3414</comment>
     </message>
     <message>
       <location filename="en/Brochure-eyes17/Back.svg"/>
       <source>The examples shown above are mostly electrical and electronics experiments but  experiments on Mechanics, Sound, Heat etc. are also documented in the user manual. The programs are written in Python, due to its simple syntax and availability of modules for scientific computation and graphics. </source>
-      <translation></translation>
+      <translation/>
       <comment>flowPara3420</comment>
     </message>
     <message>
       <location filename="en/Brochure-eyes17/Back.svg"/>
       <source>ExpEYES is supported by the PHOENIX project of Inter-University Accelerator Centre. IUAC conducts training programs for teachers, once in every six months. To be informed about the announcement of training programs and new developments, you may join the Phoenix mailing list from the website www.epeyes.in, or subscribe to the facebook page</source>
-      <translation></translation>
+      <translation/>
       <comment>flowPara3430</comment>
     </message>
     <message>
       <location filename="en/Brochure-eyes17/Back.svg"/>
       <source> The website also  contains a lot documentation on ExpEYES including videos and a user manual. It  has some  Python resources to support teaching physics and mathematics. Information on MicroHOPE, a micro-controller development kit and KuttyPy, a simple device to do  control/monitoring using python, are also available.</source>
-      <translation></translation>
+      <translation/>
       <comment>flowPara3440</comment>
     </message>
     <message>
       <location filename="en/Brochure-eyes17/Back.svg"/>
       <source>Communication to the ExpEYES hardware is done using a Python library, providing functions for operations like reading/setting voltages. The example shows the code to study an inverting amplifier. A 200 Hz sine wave is set on WG. The channels A1 and A2 are captured and plotted, using matplotlib.</source>
-      <translation></translation>
+      <translation/>
       <comment>flowPara3526</comment>
     </message>
     <message>
       <location filename="en/Brochure-eyes17/Back.svg"/>
       <source>180 degree out of phase AC voltages required for the  full wave rectifier are provided by WG and WG outputs. </source>
-      <translation></translation>
+      <translation/>
       <comment>flowPara3538</comment>
     </message>
     <message>
       <location filename="en/Brochure-eyes17/Back.svg"/>
       <source>The input waveforms are  monitored on A1 and A2 and the output on A3. A load resistor must be connected from A3 to ground to obtain proper output waveforms. A capacitor can be connected in parallel to the load</source>
-      <translation></translation>
+      <translation/>
       <comment>flowPara3542</comment>
     </message>
     <message>
       <location filename="en/Brochure-eyes17/Back.svg"/>
       <source>resistor to observe the effect of filtering. </source>
-      <translation></translation>
+      <translation/>
       <comment>flowPara3544</comment>
     </message>
     <message>
       <location filename="en/Brochure-eyes17/Back.svg"/>
       <source>The effect of LCR elements on the phase and amplitudes of an applied sine wave is studied using a series LCR circuit. The red trace is the total voltage across LC and it goes to zero at resonance (it is not exactly zero due to the resistance of the inductor coil). It can be seen that the voltages across L and C are equal and out of  phase at this point.</source>
-      <translation></translation>
+      <translation/>
       <comment>flowPara3569</comment>
     </message>
     <message>
       <location filename="en/Brochure-eyes17/Back.svg"/>
+      <source>the capacitor is captured and plotted. For 120mH and 0.1uF, the circuit is under damped as shown. The captured waveform is analyzed to extract the resonant frequency and damping factor, with an accuracy better than one percent.</source>
+      <translation/>
+      <comment>flowPara4277</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>Transistor Output Characteristics</source>
+      <translation/>
+      <comment>flowPara3372</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>Output characteristics of an NPN transistor are being obtained in this experiment. The collector voltage is swept using the programmable voltage output, PV1, and a series 1K resistor is used to limit the current as well as to act as a shunt to measure the voltage drop across the transistor's C &amp; E. Base current is set via another source, PV2, and a 100k resistor. Collector and base voltages are monitored using analog inputs A1 &amp; A2. </source>
+      <translation/>
+      <comment>flowPara3388</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>Transient  Response of LCR Circuits</source>
+      <translation/>
+      <comment>flowPara3402</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>ExpEYES can generate/monitor voltage signals as a function of time. Other physical parameters like temperature,</source>
+      <translation/>
+      <comment>flowPara3410</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>velocity, frequency  etc. also can be handled using appropriate sensor elements. Experiments are essentially a</source>
+      <translation/>
+      <comment>flowPara3412</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>sequence of these operations. Every hardware feature of ExpEYES is  accessed using a well-documented Python function call.</source>
+      <translation/>
+      <comment>flowPara3414</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>The examples shown above are mostly electrical and electronics experiments but  experiments on Mechanics, Sound, Heat etc. are also documented in the user manual. The programs are written in Python, due to its simple syntax and availability of modules for scientific computation and graphics. </source>
+      <translation/>
+      <comment>flowPara3420</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>ExpEYES is supported by the PHOENIX project of Inter-University Accelerator Centre. IUAC conducts training programs for teachers, once in every six months. To be informed about the announcement of training programs and new developments, you may join the Phoenix mailing list from the website www.epeyes.in, or subscribe to the facebook page</source>
+      <translation/>
+      <comment>flowPara3430</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source> The website also  contains a lot documentation on ExpEYES including videos and a user manual. It  has some  Python resources to support teaching physics and mathematics. Information on MicroHOPE, a micro-controller development kit and KuttyPy, a simple device to do  control/monitoring using python, are also available.</source>
+      <translation/>
+      <comment>flowPara3440</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>Communication to the ExpEYES hardware is done using a Python library, providing functions for operations like reading/setting voltages. The example shows the code to study an inverting amplifier. A 200 Hz sine wave is set on WG. The channels A1 and A2 are captured and plotted, using matplotlib.</source>
+      <translation/>
+      <comment>flowPara3526</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>180 degree out of phase AC voltages required for the  full wave rectifier are provided by WG and WG outputs. </source>
+      <translation/>
+      <comment>flowPara3538</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>The input waveforms are  monitored on A1 and A2 and the output on A3. A load resistor must be connected from A3 to ground to obtain proper output waveforms. A capacitor can be connected in parallel to the load</source>
+      <translation/>
+      <comment>flowPara3542</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>resistor to observe the effect of filtering. </source>
+      <translation/>
+      <comment>flowPara3544</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>The effect of LCR elements on the phase and amplitudes of an applied sine wave is studied using a series LCR circuit. The red trace is the total voltage across LC and it goes to zero at resonance (it is not exactly zero due to the resistance of the inductor coil). It can be seen that the voltages across L and C are equal and out of  phase at this point.</source>
+      <translation/>
+      <comment>flowPara3569</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>the capacitor is captured and plotted. For 120mH and 0.1uF, the circuit is under damped as shown. The captured waveform is analyzed to extract the resonant frequency and damping factor, with an accuracy better than one percent.</source>
+      <translation/>
+      <comment>flowPara4277</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>Transistor Output Characteristics</source>
+      <translation></translation>
+      <comment>flowPara3372</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>Output characteristics of an NPN transistor are being obtained in this experiment. The collector voltage is swept using the programmable voltage output, PV1, and a series 1K resistor is used to limit the current as well as to act as a shunt to measure the voltage drop across the transistor's C &amp; E. Base current is set via another source, PV2, and a 100k resistor. Collector and base voltages are monitored using analog inputs A1 &amp; A2. </source>
+      <translation></translation>
+      <comment>flowPara3388</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>Transient  Response of LCR Circuits</source>
+      <translation></translation>
+      <comment>flowPara3402</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>ExpEYES can generate/monitor voltage signals as a function of time. Other physical parameters like temperature,</source>
+      <translation></translation>
+      <comment>flowPara3410</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>velocity, frequency  etc. also can be handled using appropriate sensor elements. Experiments are essentially a</source>
+      <translation></translation>
+      <comment>flowPara3412</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>sequence of these operations. Every hardware feature of ExpEYES is  accessed using a well-documented Python function call.</source>
+      <translation></translation>
+      <comment>flowPara3414</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>The examples shown above are mostly electrical and electronics experiments but  experiments on Mechanics, Sound, Heat etc. are also documented in the user manual. The programs are written in Python, due to its simple syntax and availability of modules for scientific computation and graphics. </source>
+      <translation></translation>
+      <comment>flowPara3420</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>ExpEYES is supported by the PHOENIX project of Inter-University Accelerator Centre. IUAC conducts training programs for teachers, once in every six months. To be informed about the announcement of training programs and new developments, you may join the Phoenix mailing list from the website www.epeyes.in, or subscribe to the facebook page</source>
+      <translation></translation>
+      <comment>flowPara3430</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source> The website also  contains a lot documentation on ExpEYES including videos and a user manual. It  has some  Python resources to support teaching physics and mathematics. Information on MicroHOPE, a micro-controller development kit and KuttyPy, a simple device to do  control/monitoring using python, are also available.</source>
+      <translation></translation>
+      <comment>flowPara3440</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>Communication to the ExpEYES hardware is done using a Python library, providing functions for operations like reading/setting voltages. The example shows the code to study an inverting amplifier. A 200 Hz sine wave is set on WG. The channels A1 and A2 are captured and plotted, using matplotlib.</source>
+      <translation></translation>
+      <comment>flowPara3526</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>180 degree out of phase AC voltages required for the  full wave rectifier are provided by WG and WG outputs. </source>
+      <translation></translation>
+      <comment>flowPara3538</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>The input waveforms are  monitored on A1 and A2 and the output on A3. A load resistor must be connected from A3 to ground to obtain proper output waveforms. A capacitor can be connected in parallel to the load</source>
+      <translation></translation>
+      <comment>flowPara3542</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>resistor to observe the effect of filtering. </source>
+      <translation></translation>
+      <comment>flowPara3544</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
+      <source>The effect of LCR elements on the phase and amplitudes of an applied sine wave is studied using a series LCR circuit. The red trace is the total voltage across LC and it goes to zero at resonance (it is not exactly zero due to the resistance of the inductor coil). It can be seen that the voltages across L and C are equal and out of  phase at this point.</source>
+      <translation></translation>
+      <comment>flowPara3569</comment>
+    </message>
+    <message>
+      <location filename="Back.svg"/>
       <source>the capacitor is captured and plotted. For 120mH and 0.1uF, the circuit is under damped as shown. The captured waveform is analyzed to extract the resonant frequency and damping factor, with an accuracy better than one percent.</source>
       <translation></translation>
       <comment>flowPara4277</comment>
