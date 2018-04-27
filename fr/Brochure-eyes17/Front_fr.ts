8f91a7aa39900079490c11e33083840b2fb5b316
Georges Khaznadar <georgesk@debian.org>
2018-04-27 12:44:38 +0200

    French translations updated

---
+++ fr/Brochure-eyes17/Front_fr.ts
@@ -0,0 +1,205 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="Front.svg"/>
+        <source>MO</source>
+        <comment>flowPara4016</comment>
+        <translation>MO</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>PYTHON SCRIPTS</source>
+        <comment>flowPara4006</comment>
+        <translation>SCRIPTS PYTHON</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>MODULAR SENSORS</source>
+        <comment>flowPara4032</comment>
+        <translation>CAPTEURS MODULAIRES</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>OVER 50 EXPTS</source>
+        <comment>flowPara4040</comment>
+        <translation>&gt; 50 MANIPS</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>WAVE GENERATORS</source>
+        <comment>flowPara4060</comment>
+        <translation>GÉNÉRATEURS BF</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>OSCILLOSCOPE</source>
+        <comment>flowPara4068</comment>
+        <translation>OSCILLOSCOPE</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>Perform over 50 experiments!</source>
+        <comment>flowPara3575</comment>
+        <translation>Réalisez plus de 50 expériences !</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>Built-in Waveform Generator,</source>
+        <comment>flowPara3577</comment>
+        <translation>Générateur BF inclus,</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>Frequency Counter, Oscilloscope,</source>
+        <comment>flowPara3579</comment>
+        <translation>Fréquencemètre, Oscilloscope,</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>Capacitance meter and DC source.</source>
+        <comment>flowPara3581</comment>
+        <translation>Capacimètre et source de courant.</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>Accurate measurements.</source>
+        <comment>flowPara3583</comment>
+        <translation>Mesures précises.</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>Compact and USB Powered.</source>
+        <comment>flowPara3585</comment>
+        <translation>Compact et alimenté sur USB.</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>Python Programmable.</source>
+        <comment>flowPara3587</comment>
+        <translation>Programmable en Python.</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>Free &amp; Open Software &amp; Hardware.</source>
+        <comment>flowPara3589</comment>
+        <translation>Matériel et Logiciel Libres.</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>xperiments for oung ngineers and cientists</source>
+        <comment>flowPara3519</comment>
+        <translation>Experiments for Young Engineers and Scientists</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>EXPEYES enables you to develop science and engineering experiments and demonstrations without getting into the details of electronics of computer programming. GUI is available for number of experiments along with proper documentation.</source>
+        <comment>flowPara3563</comment>
+        <translation>EXPEYES permet de développer des expériences et des démonstrations de science et de technologie sans entrer dans les détails de l&apos;électronique et de la programmation. Une interface graphique est disponible pour plusieurs expériences avec sa documentation.</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>Convert your PC into a Science Laboratory</source>
+        <comment>flowPara3606</comment>
+        <translation>Convertissez votre PC en Labo de Sciences</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>- 4-Channel Oscilloscope, Upto 1MSPS</source>
+        <comment>flowPara3654</comment>
+        <translation>- Oscilloscope 4 canaux, 1 Méch/s</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>- 12-bit Voltmeter, ±16V Range</source>
+        <comment>flowPara3656</comment>
+        <translation>- Volmètre 12 bits, calibre ±16V</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>- Programmable voltage sources :±5,3.3V</source>
+        <comment>flowPara3658</comment>
+        <translation>- Sources de tension programmables : ±5 ; 3,3 V</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>- Constant current source : 1.1mA</source>
+        <comment>flowPara3660</comment>
+        <translation>- Source de courant constant : 1,1 mA</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>- Frequency counter and timing tools</source>
+        <comment>flowPara3662</comment>
+        <translation>- Fréquencemètre et outils de comptage</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>- Function generators: 5Hz to 5KHz. ±3V</source>
+        <comment>flowPara3664</comment>
+        <translation>- Générateur BF : 5 Hz à 5 kHz ±3 V</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>  with Inverted Output</source>
+        <comment>flowPara3666</comment>
+        <translation>  avec sortie inversée</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>- 2x Square wave generators. Upto 1MHz</source>
+        <comment>flowPara3668</comment>
+        <translation>- 2x signaux carrés. Jusqu&apos;à 1MHz</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>- I2C expansion bus for sensors</source>
+        <comment>flowPara3670</comment>
+        <translation>- Bus d&apos;extension I2C pour capteurs</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>- 50ppm/°C analog stability</source>
+        <comment>flowPara3672</comment>
+        <translation>- stabilité analogique 50ppm/°C</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>- Capacitance Meter : 10pF-100uF</source>
+        <comment>flowPara3674</comment>
+        <translation>- Capacimètre : 10 pF - 100 µF</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>- Python Programmable </source>
+        <comment>flowPara3676</comment>
+        <translation>- Programmable en Python </translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>INTER-UNIVERSITY ACCELERATOR CENTRE</source>
+        <comment>flowPara3686</comment>
+        <translation>INTER-UNIVERSITY ACCELERATOR CENTRE</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>(An autonomous research facility of UGC)</source>
+        <comment>flowPara3688</comment>
+        <translation>(service de recherche autonome de l&apos;UGC)</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>Aruna Asaf Ali Marg, New Delhi 110 067</source>
+        <comment>flowPara3690</comment>
+        <translation>Aruna Asaf Ali Marg, New Delhi 110 067</translation>
+    </message>
+    <message>
+        <location filename="Front.svg"/>
+        <source>www.iuac.res.in  |  www.expeyes.in  |  expeyes@gmail.com</source>
+        <comment>flowPara3692</comment>
+        <translation>www.iuac.res.in  |  www.expeyes.in  |  expeyes@gmail.com</translation>
+    </message>
+</context>
+</TS>
