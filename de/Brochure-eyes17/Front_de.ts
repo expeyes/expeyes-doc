0cb0540bfe03ebc779f941c14bac78a1f55eddcf
Georges Khaznadar <georgesk@debian.org>
2018-04-27 18:40:43 +0200

    initialized de, es and nl brochures

---
+++ de/Brochure-eyes17/Front_de.ts
@@ -0,0 +1,241 @@
+<?xml version="1.0" ?>
+<!DOCTYPE TS>
+<TS language="fr_FR" version="2.1">
+  <context>
+    <name>@default</name>
+    <message>
+      <location filename="Front.svg"/>
+      <source>MO</source>
+      <translation></translation>
+      <comment>flowPara4016</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>PYTHON SCRIPTS</source>
+      <translation></translation>
+      <comment>flowPara4006</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>MODULAR SENSORS</source>
+      <translation></translation>
+      <comment>flowPara4032</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>OVER 50 EXPTS</source>
+      <translation></translation>
+      <comment>flowPara4040</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>WAVE GENERATORS</source>
+      <translation></translation>
+      <comment>flowPara4060</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>OSCILLOSCOPE</source>
+      <translation></translation>
+      <comment>flowPara4068</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>Perform over 50 experiments!</source>
+      <translation></translation>
+      <comment>flowPara3575</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>Built-in Waveform Generator,</source>
+      <translation></translation>
+      <comment>flowPara3577</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>Frequency Counter, Oscilloscope,</source>
+      <translation></translation>
+      <comment>flowPara3579</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>Capacitance meter and DC source.</source>
+      <translation></translation>
+      <comment>flowPara3581</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>Accurate measurements.</source>
+      <translation></translation>
+      <comment>flowPara3583</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>Compact and USB Powered.</source>
+      <translation></translation>
+      <comment>flowPara3585</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>Python Programmable.</source>
+      <translation></translation>
+      <comment>flowPara3587</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>Free &amp; Open Software &amp; Hardware.</source>
+      <translation></translation>
+      <comment>flowPara3589</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>xperiments for oung ngineers and cientists</source>
+      <translation></translation>
+      <comment>flowPara3519</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>EXPEYES enables you to develop science and engineering experiments and demonstrations without getting into the details of electronics of computer programming. GUI is available for number of experiments along with proper documentation.</source>
+      <translation></translation>
+      <comment>flowPara3563</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>Convert your PC into a Science Laboratory</source>
+      <translation></translation>
+      <comment>flowPara3606</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>- 4-Channel Oscilloscope, Upto 1MSPS</source>
+      <translation></translation>
+      <comment>flowPara3654</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>- 12-bit Voltmeter, ±16V Range</source>
+      <translation></translation>
+      <comment>flowPara3656</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>- Programmable voltage sources :±5,3.3V</source>
+      <translation></translation>
+      <comment>flowPara3658</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>- Constant current source : 1.1mA</source>
+      <translation></translation>
+      <comment>flowPara3660</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>- Frequency counter and timing tools</source>
+      <translation></translation>
+      <comment>flowPara3662</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>- Function generators: 5Hz to 5KHz. ±3V</source>
+      <translation></translation>
+      <comment>flowPara3664</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>  with Inverted Output</source>
+      <translation></translation>
+      <comment>flowPara3666</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>- 2x Square wave generators. Upto 1MHz</source>
+      <translation></translation>
+      <comment>flowPara3668</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>- I2C expansion bus for sensors</source>
+      <translation></translation>
+      <comment>flowPara3670</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>- 50ppm/°C analog stability</source>
+      <translation></translation>
+      <comment>flowPara3672</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>- Capacitance Meter : 10pF-100uF</source>
+      <translation></translation>
+      <comment>flowPara3674</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>- Python Programmable </source>
+      <translation></translation>
+      <comment>flowPara3676</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>INTER-UNIVERSITY ACCELERATOR CENTRE</source>
+      <translation></translation>
+      <comment>flowPara3686</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>(An autonomous research facility of UGC)</source>
+      <translation></translation>
+      <comment>flowPara3688</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>Aruna Asaf Ali Marg, New Delhi 110 067</source>
+      <translation></translation>
+      <comment>flowPara3690</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>www.iuac.res.in  |  www.expeyes.in  |  expeyes@gmail.com</source>
+      <translation></translation>
+      <comment>flowPara3692</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>Discover the</source>
+      <translation></translation>
+      <comment>tspan3539</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source> Joy of </source>
+      <translation></translation>
+      <comment>tspan3541</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>Science!</source>
+      <translation></translation>
+      <comment>tspan3543</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>Like us on Facebook!</source>
+      <translation></translation>
+      <comment>tspan3614</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>User Friendy GUI</source>
+      <translation></translation>
+      <comment>tspan3642</comment>
+    </message>
+    <message>
+      <location filename="Front.svg"/>
+      <source>FEATURES</source>
+      <translation></translation>
+      <comment>tspan3646</comment>
+    </message>
+  </context>
+</TS>
---
+++ es/Brochure-eyes17/Back_es.ts
@@ -0,0 +1,151 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="Back.svg"/>
+        <source>Transistor Output Characteristics</source>
+        <comment>flowPara3372</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source>Output characteristics of an NPN transistor are being obtained in this experiment. The collector voltage is swept using the programmable voltage output, PV1, and a series 1K resistor is used to limit the current as well as to act as a shunt to measure the voltage drop across the transistor&apos;s C &amp; E. Base current is set via another source, PV2, and a 100k resistor. Collector and base voltages are monitored using analog inputs A1 &amp; A2. </source>
+        <comment>flowPara3388</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source>Transient  Response of LCR Circuits</source>
+        <comment>flowPara3402</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source>ExpEYES can generate/monitor voltage signals as a function of time. Other physical parameters like temperature, velocity, frequency  etc. also can be handled using appropriate sensor elements. Experiments are essentially a sequence of these operations. Every hardware feature of ExpEYES is  accessed using a well-documented Python function call.</source>
+        <comment>flowPara3410</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source> </source>
+        <comment>flowPara4384</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source>The examples shown above are mostly electrical and electronics experiments but  experiments on Mechanics, Sound, Heat etc. are also documented in the user manual. The programs are written in Python, due to its simple syntax and availability of modules for scientific computation and graphics. </source>
+        <comment>flowPara3420</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source> </source>
+        <comment>flowPara3422</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source>ExpEYES is supported by the PHOENIX project of Inter-University Accelerator Centre. IUAC conducts training programs for teachers, once in every six months. To be informed about the announcement of training programs and new developments, you may join the Phoenix mailing list from the website www.epeyes.in, or subscribe to the facebook page</source>
+        <comment>flowPara3430</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source> </source>
+        <comment>flowPara3432</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source> The website also  contains a lot documentation on ExpEYES including videos and a user manual. It  has some  Python resources to support teaching physics and mathematics. Information on MicroHOPE, a micro-controller development kit and KuttyPy, a simple device to do  control/monitoring using python, are also available.</source>
+        <comment>flowPara3440</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source> </source>
+        <comment>flowPara3442</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source> </source>
+        <comment>flowPara3496</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source> </source>
+        <comment>flowPara3514</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source>Communication to the ExpEYES hardware is done using a Python library, providing functions for operations like reading/setting voltages. The example shows the code to study an inverting amplifier. A 200 Hz sine wave is set on WG. The channels A1 and A2 are captured and plotted, using matplotlib.</source>
+        <comment>flowPara3526</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source>180 degree out of phase AC voltages required for the  full wave rectifier are provided by WG and WG outputs. The input waveforms are  monitored on A1 and A2 and the output on A3. A load resistor must be connected from A3 to ground to obtain proper output waveforms. A capacitor can be connected in parallel to the load resistor to observe the effect of filtering. </source>
+        <comment>flowPara3538</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source>The effect of LCR elements on the phase and amplitudes of an applied sine wave is studied using a series LCR circuit. The red trace is the total voltage across LC and it goes to zero at resonance (it is not exactly zero due to the resistance of the inductor coil). It can be seen that the voltages across L and C are equal and out of  phase at this point.</source>
+        <comment>flowPara3569</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source> </source>
+        <comment>flowPara3571</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source> </source>
+        <comment>flowPara3579</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source> </source>
+        <comment>flowPara4255</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source>The transient response of an LCR circuit is shown here. A five to zero volt step is applied across the inductor (a 3000 turn solenoid) , and the resultant voltage variation across the capacitor is captured and plotted. For 120mH and 0.1uF, the circuit is under damped as shown. The captured waveform is analyzed to extract the resonant frequency and damping factor, with an accuracy better than one percent.  </source>
+        <comment>flowPara4277</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source>Programming ExpEYES using Python</source>
+        <comment>tspan3460</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source>Full Wave Rectifier</source>
+        <comment>tspan3464</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source>RLC Steady state response</source>
+        <comment>tspan3468</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Back.svg"/>
+        <source> </source>
+        <comment>tspan3480</comment>
+        <translation></translation>
+    </message>
+</context>
+</TS>
